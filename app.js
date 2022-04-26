7e2c3529f111fee1630609bc4f1123f6a55a2ac1
Felipe Reinaux <felipereinaux@gmail.com>
2022-04-26 16:11:28 -0300

    whatsapp integration

--- app.js
+++ app.js
@@ -2,6 +2,45 @@ const express = require('express')
 const CepCoords = require("coordenadas-do-cep")
 const cors = require('cors');
 const app = express()
+const fs = require('fs');
+const venom = require('venom-bot');
+let localClient
+
+venom
+  .create(
+    'reinaux',
+    (base64Qr, asciiQR, attempts, urlCode) => {
+      console.log(asciiQR); // Optional to log the QR in the terminal
+      var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+        response = {};
+
+      if (matches.length !== 3) {
+        return new Error('Invalid input string');
+      }
+      response.type = matches[1];
+      response.data = new Buffer.from(matches[2], 'base64');
+
+      var imageBuffer = response;
+      require('fs').writeFile(
+        'qr.png',
+        imageBuffer['data'],
+        'binary',
+        function (err) {
+          if (err != null) {
+            console.log(err);
+          }
+        }
+      );
+    },
+    undefined,
+    { logQR: false }
+  )
+  .then((client) => {
+    localClient = client;
+  })
+  .catch((erro) => {
+    console.log(erro);
+  });
 
 app.use(express.json())
 app.use(cors({
@@ -10,6 +49,17 @@ app.use(cors({
 const port = process.env.PORT ||3000
 
 app.get('',(req,res) => { res.send('ok') })
+
+app.get('/whatsapp/qr', async (req, res) => {
+	res.sendFile('qr.png', { root: __dirname })
+})
+
+app.post('/whatsapp/send', async (req, res) => {
+	let body = req.body
+
+  localClient.sendText(body.Fone + '@c.us',body.Mensagem)
+  res.send(200)
+})
 
 app.get('/cep/:cep', async (req, res) => {
   const info = await CepCoords.getByCep(req.params.cep);
